38dbe51a7dfc8b3fdde9f8b7c3430c82ac949d3b
NikitaZotov <nikita.zotov.belarus@gmail.com>
2024-09-08 16:13:40 +0300

    [refactor][builder][gwf] Separate definitions and implementations; fix variable namings

--- sc-tools/sc-builder/src/gwf_translator.hpp
+++ sc-tools/sc-builder/src/gwf_translator.hpp
@@ -6,17 +6,12 @@
 
 #pragma once
 
-#include "translator.hpp"
-#include "scs_translator.hpp"
-#include "builder_defines.hpp"
-
-#include "gwf_parser.hpp"
-#include "sc_scs_writer.hpp"
-#include "gwf_translator_const.hpp"
-
 #include <vector>
 #include <string>
 #include <list>
+
+#include "translator.hpp"
+#include "scs_translator.hpp"
 
 class GWFTranslator : public Translator
 {
